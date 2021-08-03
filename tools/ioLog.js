2d1a992e65bae1357ee5f26e0197b6653a7f8a94
SatomiKazeHiro <tonasgtl@gmail.com>
2021-08-03 22:07:07 +0800

    重写之前的备份 - 2021年08月03日 22点06分

--- tools/ioLog.js
+++ tools/ioLog.js
@@ -6,34 +6,39 @@ const { appendFileSync } = require('fs');
 
 // 自定义打印加追加文本方法
 /**
- * log输出，保存log信息
+ * 输出log并保存log信息
  * @param {String} path i/o log配置文件的路径
  * @param {String} msg 需要输出的信息
  * @param {String} type 输出信息类型
  */
 let ioLog = (path, msg, type) => {
+  // 生成时间
   let time = new Date();
+  // 开头格式字符串
   let startStr = '     - ';
+  // 保存内容模板[INFO]
+  let logINFO = `[INFO][${time}] ${msg}${EOL}`;
+  // 判断输出的消息的类型
   if (!type) {
-    // 输出一般信息
+    // 没有指定类型则输出一般信息
     console.log(startStr + msg);
-    appendFileSync(path, '[INFO][' + time + '] ' + msg + EOL, 'utf8');
+    appendFileSync(path, logINFO, 'utf8');
   } else if (type.toLowerCase() == 'increase') {
     // 输出系统目录增加的信息
     console.log(startStr + '\033[40;36m' + msg + '\033[0m');
-    appendFileSync(path, '[INFO][' + time + '] ' + msg + EOL, 'utf8');
+    appendFileSync(path, logINFO, 'utf8');
   } else if (type.toLowerCase() == 'decrease') {
     // 输出系统目录被移除的信息
     console.log(startStr + '\033[40;31m' + msg + '\033[0m');
-    appendFileSync(path, '[INFO][' + time + '] ' + msg + EOL, 'utf8');
+    appendFileSync(path, logINFO, 'utf8');
   } else if (type.toLowerCase() == 'up') {
     // 输出目录内容成功装载的信息
     console.log(startStr + '\033[40;32m' + msg + '\033[0m');
-    appendFileSync(path, '[INFO][' + time + '] ' + msg + EOL, 'utf8');
+    appendFileSync(path, logINFO, 'utf8');
   } else if (type.toLowerCase() == 'warning') {
     // 输出目录内容成功装载的信息
     console.log(startStr + '\033[40;33m' + msg + '\033[0m');
-    appendFileSync(path, '[INFO][' + time + '] ' + msg + EOL, 'utf8');
+    appendFileSync(path, logINFO, 'utf8');
   } else if (type.toLowerCase() == 'done') {
     // 输出服务器启动的信息
     console.log('\033[42;30m DONE \033[40;32m 服务器启动成功 http://www.localhost:' + msg + '\033[0m');
